bdabe19620271336d6cc2b0da4a78e8a64647c38
Sagar Bharati <bharatisagar678@gmail.com>
2024-09-20 13:45:54 +0530

    sagar

--- frontend/src/components/CustomButton.jsx
+++ frontend/src/components/CustomButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './button.css'
 
-const CustomButton = ({text,size,handleClick}) => {
+const CustomButton = ({text,handleClick}) => {
     
   return (
     <button onClick={handleClick} className='btn btn-lg'><b style={{color:"white"}}>{text}</b></button>
--- frontend/src/components/LiveResume.jsx
+++ frontend/src/components/LiveResume.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import CustomButton from './CustomButton';
+import html2pdf from 'html2pdf.js';
 
 import Temp1 from '../ResumeTemplates/template1/Temp1'
 import Temp2 from '../ResumeTemplates/template2/Temp2'
@@ -9,6 +11,19 @@ import Temp5 from '../ResumeTemplates/template5/Temp5'
 import Temp6 from '../ResumeTemplates/template6/Temp6'
 
 const LiveResume = () => {
+  const handleDownload = () => {
+    const element = document.querySelector('.resumes'); // Selects the element with 'resumes' class
+
+    if (element) {
+      html2pdf()
+        .from(element)
+        .set({
+          filename: 'resume.pdf', // File name for the downloaded PDF
+          jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+        })
+        .save();
+    }
+  };
     const { rid } = useParams();
   const components = {
     1: Temp1,
@@ -20,10 +35,11 @@ const LiveResume = () => {
   };
 
   const Component = components[Number(rid)];
-
+  
   return (
     <div>
       {Component && <Component />}
+      <CustomButton handleClick={handleDownload} text="download"/>    
     </div>
   );
 };
